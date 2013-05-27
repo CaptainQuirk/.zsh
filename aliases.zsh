321d746f09e839b92682babedc86a1aa65903f18
CaptainQuirk <leonard.messier@gmail.com>
2013-05-27 15:26:09 +0200

    [feature] alias for ack command

--- aliases.zsh
+++ aliases.zsh
@@ -21,3 +21,5 @@ alias gcl='git clone'
 alias gta='git tag -a -m'
 alias gf='git reflog'
 alias gv='git log --pretty=format:'%s' | cut -d " " -f 1 | sort | uniq -c | sort -nr'
+
+alias ack="ack-grep"
