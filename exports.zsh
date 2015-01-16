752e4b304139d5fc0d8b11947dd3fdd33b38274e
lmessier <leonard.messier@gmail.com>
2015-01-16 14:27:45 +0100

    [+] including composer bin in path

--- exports.zsh
+++ exports.zsh
@@ -1,3 +1,3 @@
 export ACKRC=".ackrc"
-export PATH=/opt/local/bin:/opt/local/sbin:~/bin:$PATH
+export PATH=/opt/local/bin:/opt/local/sbin:~/bin:/~/.composer/vendor/bin:$PATH
 export MANPATH=/opt/local/share/man:$MANPATH
