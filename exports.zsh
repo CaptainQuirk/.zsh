147205d024bc9cbe9e3c9e2fdcd7ce7dff92fb42
lmessier <leonard.messier@gmail.com>
2016-01-18 14:05:19 +0100

    (master) Adding some os x export vars

--- exports.zsh
+++ exports.zsh
@@ -1,3 +1,5 @@
 export ACKRC=".ackrc"
-export PATH=~/.android/android-sdk/platform-tools/:~/.android/android-sdk/tools/:/opt/local/bin:/opt/local/sbin:~/bin:/~/.composer/vendor/bin:$PATH
+export PATH=~/npm/bin:~/Documents/System/gitextensions:~/.android/android-sdk/platform-tools:~/.android/android-sdk/tools:/opt/local/bin:/opt/local/sbin:~/bin:/~/.composer/vendor/bin:$PATH
 export MANPATH=/opt/local/share/man:$MANPATH
+export PYTHONPATH=/usr/local/bin:~/.powerline/plugins:$PYTHONPATH
+
