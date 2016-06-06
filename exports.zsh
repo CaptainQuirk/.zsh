97c916933cf0c004b9d1622468b781a8bdc147c7
lmessier <leonard.messier@gmail.com>
2016-06-06 17:14:25 +0200

    (master) Set LESSCHARSET and LC_ALL

--- exports.zsh
+++ exports.zsh
@@ -4,3 +4,10 @@ export MANPATH=/opt/local/share/man:$MANPATH
 export PYTHONPATH=/usr/local/bin:~/.powerline/plugins:$PYTHONPATH
 export GOPATH=~/.go
 
+if [[ $IS_MAC -eq 1 ]]; then
+  export LESSCHARSET=next
+else
+  export LESSCHARSET=utf-8
+fi
+
+export LC_ALL=en_GB.UTF-8
