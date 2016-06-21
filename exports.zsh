4803322aecc0ecd0627ccddb1a38b803fcaa0529
lmessier <leonard.messier@gmail.com>
2016-06-21 16:00:43 +0200

    add linuxbrew to path

--- exports.zsh
+++ exports.zsh
@@ -1,5 +1,5 @@
 export ACKRC=".ackrc"
-export PATH=~/npm/bin:~/Documents/System/gitextensions:~/.android/android-sdk/platform-tools:~/.android/android-sdk/tools:/opt/local/bin:/opt/local/sbin:~/bin:/~/.composer/vendor/bin:$PATH
+export PATH=~/npm/bin:~/Documents/System/gitextensions:~/.android/android-sdk/platform-tools:~/.android/android-sdk/tools:/opt/local/bin:/opt/local/sbin:~/bin:/~/.composer/vendor/bin:~/.linuxbrew/bin:$PATH
 export MANPATH=/opt/local/share/man:$MANPATH
 export PYTHONPATH=/usr/local/bin:~/.powerline/plugins:$PYTHONPATH
 export GOPATH=~/.go
